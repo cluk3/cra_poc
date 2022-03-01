d91d3a3544ab3df2e8f6e89d321a824e9baf913a
Luca Campli <luca@captivateiq.com>
2022-03-01 12:23:49 -0500

    add lorem ipsum

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Heading, Spinner, Center } from "@chakra-ui/react";
+import { Heading, Spinner, Center, Text, Box } from "@chakra-ui/react";
 
 const About = () => {
   const [isReady, setIsReady] = useState(false);
@@ -10,7 +10,18 @@ const About = () => {
   }, []);
 
   return isReady ? (
-    <Heading>About</Heading>
+    <Box>
+      <Heading>About</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
+    </Box>
   ) : (
     <Center h="200px">
       <Spinner size="sm" /> Fetching...
